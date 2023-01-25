989a603a7effea25525dc52da83f6248d17712a0
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-25 08:50:20 -0400

    Initial commit

---
+++ TextRPG/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Player
+    {
+        int x;
+        int y;
+
+        public Player(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+    }
+}
