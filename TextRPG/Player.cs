66dd6a26861fe204442b02029c6fae0c49357785
Ryan Wohler <ryanwohler55@gmail.com>
2023-01-25 09:01:32 -0400

    Mapless Movement

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -8,13 +8,41 @@ namespace TextRPG
 {
     internal class Player
     {
-        int x;
-        int y;
+        public int x;
+        public int y;
+        public char sprite;
 
-        public Player(int x, int y)
+        public Player(int x, int y, char sprite)
         {
             this.x = x;
             this.y = y;
+            this.sprite = sprite;
+            Console.SetCursorPosition(this.x, this.y);
+            Console.Write(sprite);
+        }
+
+        public void Move(ConsoleKey key)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(' ');
+            if(key == ConsoleKey.W || key == ConsoleKey.UpArrow)
+            {
+                y--;
+            }
+            if (key == ConsoleKey.S || key == ConsoleKey.DownArrow)
+            {
+                y++;
+            }
+            if (key == ConsoleKey.A || key == ConsoleKey.LeftArrow)
+            {
+                x--;
+            }
+            if (key == ConsoleKey.D || key == ConsoleKey.RightArrow)
+            {
+                x++;
+            }
+            Console.SetCursorPosition(x, y);
+            Console.Write(sprite);
         }
 
 
--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -8,9 +8,29 @@ namespace TextRPG
 {
     internal class Program
     {
+
+        static bool play = true;
+        static ConsoleKey key;
+
+        static Player player = new Player(5, 5, 'X');
+        
+
+
         static void Main(string[] args)
         {
-
+            Console.CursorVisible = false;
+            while (play)
+            {
+                key = Console.ReadKey(true).Key;
+                if(key == ConsoleKey.Escape)
+                {
+                    play = false;
+                }
+                else
+                {
+                    player.Move(key);
+                }
+            }
 
         }
     }
